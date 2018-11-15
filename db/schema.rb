819b7984a12077819dc5568e433baa83ea34885a
Alexander Weir <CryptoPynchon@gmail.com>
2018-11-15 14:21:35 -0500

    maps on index and show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_213354) do
+ActiveRecord::Schema.define(version: 2018_11_15_145847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2018_11_12_213354) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
