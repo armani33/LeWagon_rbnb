f188a71d61db0fad0755c0d9c2243e5367d59240
alex george <alexandreantoinegeorge@gmail.com>
2018-11-16 10:49:24 -0500

    user_image added - Pb with display in the user_show_page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,8 @@ class DashboardController < ApplicationController
   def show
     @user = current_user
   end
+
+  def article_params
+    params.require(:user).permit(:photo)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_210430) do
+ActiveRecord::Schema.define(version: 2018_11_15_223006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2018_11_15_210430) do
     t.string "category"
     t.text "description"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
@@ -69,6 +71,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_210430) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
